2575c7edf72605716faa1b4c4b092de3fdaef97e
Nesterov Nikita <shadr.nn@gmail.com>
2025-11-10 05:21:29 +0300

    request completions asynchronously

--- tailwind-cache.el
+++ tailwind-cache.el
@@ -69,7 +69,7 @@
               :experimental (:classRegex ["class: \"(.*)\""])
               :classAttributes ["class" "className" "ngClass" "class:list"]))))))
 
-(defun tailwind-minor-mode--get-completions (root)
+(defun tailwind-minor-mode--get-completions (root CALLBACK)
   (let ((tailwind-cache--connection (tailwind-minor-mode--cache-make-connection)))
     (jsonrpc-request tailwind-cache--connection 'initialize `((processId) (rootPath . ,root) (rootUri . ,root) (capabilities (general (positionEncodings . ["utf-32" "utf-16"])) (workspace (workspaceEdit (documentChanges . t) (resourceOperations . ["create" "rename" "delete"])) (applyEdit . t) (symbol (symbolKind (valueSet . [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26]))) (executeCommand (dynamicRegistration . :json-false)) (didChangeWatchedFiles (dynamicRegistration . t)) (workspaceFolders . t) (configuration . t) (inlayHint (refreshSupport . :json-false)) (diagnostics (refreshSupport . :json-false)) (fileOperations (didCreate . :json-false) (willCreate . :json-false) (didRename . t) (willRename . t) (didDelete . :json-false) (willDelete . :json-false))) (textDocument (declaration (dynamicRegistration . t) (linkSupport . t)) (definition (dynamicRegistration . t) (linkSupport . t)) (references (dynamicRegistration . t)) (implementation (dynamicRegistration . t) (linkSupport . t)) (typeDefinition (dynamicRegistration . t) (linkSupport . t)) (synchronization (willSave . t) (didSave . t) (willSaveWaitUntil . t)) (documentSymbol (symbolKind (valueSet . [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26])) (hierarchicalDocumentSymbolSupport . t)) (formatting (dynamicRegistration . t)) (rangeFormatting (dynamicRegistration . t)) (onTypeFormatting (dynamicRegistration . t)) (rename (dynamicRegistration . t) (prepareSupport . t)) (codeAction (dynamicRegistration . t) (isPreferredSupport . t) (codeActionLiteralSupport (codeActionKind (valueSet . ["" "quickfix" "refactor" "refactor.extract" "refactor.inline" "refactor.rewrite" "source" "source.organizeImports"]))) (resolveSupport (properties . ["edit" "command"])) (dataSupport . t)) (completion (completionItem (snippetSupport . :json-false) (documentationFormat . ["markdown" "plaintext"]) (resolveAdditionalTextEditsSupport . t) (insertReplaceSupport . t) (deprecatedSupport . t) (resolveSupport (properties . ["documentation" "detail" "additionalTextEdits" "command"])) (insertTextModeSupport (valueSet . [1 2])) (labelDetailsSupport . t)) (contextSupport . t) (dynamicRegistration . t)) (signatureHelp (signatureInformation (parameterInformation (labelOffsetSupport . t)) (activeParameterSupport . t)) (dynamicRegistration . t)) (documentLink (dynamicRegistration . t) (tooltipSupport . t)) (hover (contentFormat . ["markdown" "plaintext"]) (dynamicRegistration . t)) (selectionRange (dynamicRegistration . t)) (callHierarchy (dynamicRegistration . :json-false)) (typeHierarchy (dynamicRegistration . t)) (publishDiagnostics (relatedInformation . t) (tagSupport (valueSet . [1 2])) (versionSupport . t)) (diagnostic (dynamicRegistration . :json-false) (relatedDocumentSupport . :json-false)) (linkedEditingRange (dynamicRegistration . t)) (inlineCompletion) (inlayHint (dynamicRegistration . :json-false))) (window (workDoneProgress . t) (showDocument (support . t)))) (initializationOptions (configuration (tailwindCSS (emmetCompletions . t) (showPixelEquivalents . t) (rootFontSize . 16) (validate . t) (hovers . :json-false) (suggestions . t) (codeActions . :json-false) (lint (invalidScreen . "error") (invalidVariant . "error") (invalidTailwindDirective . "error") (invalidApply . "error") (invalidConfigPath . "error") (cssConflict . "warning") (recommendedVariantOrder . "warning")) (experimental (classRegex . ["class: \"(.*)\""])) (classAttributes . ["class" "className" "ngClass" "class:list"])))) (workDoneToken . "1")))
     (jsonrpc-notify tailwind-cache--connection 'initialized '())
@@ -80,12 +80,14 @@
                        :version 1
                        :text "<div class=\"\"></div>\n")))
 
-    (let ((result (jsonrpc-request tailwind-cache--connection 'textDocument/completion
-                                   `(:textDocument (:uri ,(concat "file://" root "/.tailwind-cache.html"))
-                                     :position (:line 0 :character 12)
-                                     :context (:triggerKind 1)))))
-      (jsonrpc-shutdown tailwind-cache--connection)
-      (plist-get result :items))))
+    (jsonrpc-async-request tailwind-cache--connection
+                           'textDocument/completion
+                           `(:textDocument (:uri ,(concat "file://" root "/.tailwind-cache.html"))
+                             :position (:line 0 :character 12)
+                             :context (:triggerKind 1))
+                           :success-fn (lambda (result)
+                                         (jsonrpc-shutdown tailwind-cache--connection)
+                                         (funcall CALLBACK (plist-get result :items))))))
 
 (defun tailwind-minor-mode--completions-to-hash-table (sequence)
   (let ((hash-table (make-hash-table :test 'equal :size (length sequence))))
@@ -100,11 +102,16 @@
     (plist-get tailwind-minor-mode--cached-completions root #'string-equal)))
 
 (defun tailwind-minor-mode-cache-completions ()
+  "Caches tailwindcss class completions for the current project."
   (interactive)
   (let ((root (project-root (project-current))))
-    (setq tailwind-minor-mode--cached-completions
-          (plist-put tailwind-minor-mode--cached-completions root
-                     (tailwind-minor-mode--completions-to-hash-table (tailwind-minor-mode--get-completions root))))))
+    (tailwind-minor-mode--get-completions
+     root
+     (lambda (result)
+       (setq tailwind-minor-mode--cached-completions
+             (plist-put tailwind-minor-mode--cached-completions root
+                        (tailwind-minor-mode--completions-to-hash-table result)))))))
+
 
 (provide 'tailwind-cache)
 ;;; tailwind-cache.el ends here
